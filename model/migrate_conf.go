6b06fecf86931870440bb6bacc62acd2a517fbc4
小影 <soseek@vip.qq.com>
2021-03-19 12:07:01 +0000

    refactor: 调整结构

--- model/migrate_conf.go
+++ model/migrate_conf.go
@@ -41,23 +41,3 @@ func AutoMigrateTable() []interface{} {
 	}
 }
 
-var migrateData []func() (key string, exec func(db *gorm.DB) error)
-
-func AutoMigrateData() []func() (key string, exec func(db *gorm.DB) error) {
-
-	// 需要自动创建初始化执行的操作，key 是唯一
-	// 🙅 不要修改历史数据！不要修改历史数据！不要修改历史数据！
-
-	migrateData = append(migrateData, func() (string, func(db *gorm.DB) error) {
-		return "FirstAutoMigrateData", // Key
-			func(db *gorm.DB) error { // 数据处理
-				db.Create(&MigrateLogs{
-					Name: "AutoMigrateData",
-				})
-				return nil
-			}
-	})
-
-	_ = zutil.RunAllMethod(&migrate{})
-	return migrateData
-}
---
+++ model/migrate_data.go
@@ -0,0 +1,28 @@
+package model
+
+import (
+	"github.com/sohaha/zlsgo/zutil"
+	"gorm.io/gorm"
+)
+
+var migrateData []func() (key string, exec func(db *gorm.DB) error)
+
+func AutoMigrateData() []func() (key string, exec func(db *gorm.DB) error) {
+
+	// 需要自动创建初始化执行的操作，key 是唯一
+	// 🙅 不要修改历史数据！不要修改历史数据！不要修改历史数据！
+
+	migrateData = append(migrateData, func() (string, func(db *gorm.DB) error) {
+		return "FirstAutoMigrateData", // Key
+			func(db *gorm.DB) error { // 数据处理
+				db.Create(&MigrateLogs{
+					Name: "AutoMigrateData",
+				})
+				return nil
+			}
+	})
+
+	_ = zutil.RunAllMethod(&migrate{})
+	return migrateData
+}
+
